6cb26fa9f9482c701c5bf33de34f472b9f1adc0e
Mai Thi Kim Dung <dungmtk@vmogroup.com>
2024-04-23 10:14:24 +0700

    [feat] qdrant connection

--- src/config/qdrant_cfg.py
+++ src/config/qdrant_cfg.py
@@ -7,6 +7,6 @@ load_dotenv()
 @dataclass
 class QdrantDBInfo:
     path_qdrant_db: str = os.getenv("QDRANT_URL")
-    embed_size = 786
+    embed_size = 3
     # The most typical metric used in similarity learning models is the cosine metric
     metric_type = "COSINE"
--- src/external_access/qdrant_connect.py
+++ src/external_access/qdrant_connect.py
@@ -15,8 +15,8 @@ class QdrantConnection:
     def connect_qdrant(self):
         try: 
             return QdrantClient(QdrantDBInfo.path_qdrant_db)
-        except Exception:
-            logger.exception("Connect to Qdrant fail!")
+        except Exception as e:
+            logger.exception(f"Connect to Qdrant fail: {e}")
 
     def create_collection(self, collection_name):
         try:
@@ -26,24 +26,47 @@ class QdrantConnection:
                     size=QdrantDBInfo.embed_size, distance=self.distance
                 ),
             )
-        except Exception:
-            logger.exception("Create collection failed!")
-
+            logger.info(f"Collection {collection_name} created successfully!")
+            return True
+        except Exception as e:
+            logger.exception(f"Create collection failed: {e}")
+        return False
+    
     def upsert_data(self, collection_name, data):
-        self.client.upsert(
-            collection_name=collection_name,
-            points=models.Batch(
-                **data
-            ),
-        )
+        try:
+            response = self.client.upsert(
+                collection_name=collection_name,
+                points=models.Batch(**data),
+            )
+            if response.status == "completed":
+                logger.info("Upsert data successfully!")
+                return True
+        except Exception as e:
+            logger.exception("Error while upserting data: %s", e)
+        logger.info("Upsert data failed!")
+        return False
 
     def get_collection(self, collection_name):
         try: 
             return self.client.get_collection(collection_name=collection_name)
         except Exception:
             logger.exception(f"Get collection {collection_name} failed!")
+        return None
 
 
 if __name__=="__main__":
     qdrant_connection = QdrantConnection()
     qdrant_connection.create_collection("test")
+    batch_data = {
+        "ids": [1, 2, 3],
+        "vectors": [[0.1, 0.2, 0.4], [0.3, 0.4, 0.5], [0.5, 0.6, 0.7]],
+        "payloads": [
+            {
+                "text": "a",
+                "title": "b",
+                "url": "c",
+            }
+            for _ in range(0,3)
+        ],
+    }
+    qdrant_connection.upsert_data("test", batch_data)
